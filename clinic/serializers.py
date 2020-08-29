2412b9b1340d5680ee73a91e9b622e70f43e489f
Mohammad Salehi <msalehy77@gmail.com>
2020-08-29 18:43:30 +0430

    compatible with frontend

--- clinic/serializers.py
+++ clinic/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class ClinicSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ClinicModel
-        fields = '__all__'
+        fields = ('id', 'clinicName', 'diagramModel', 'diagramTree', 'modified_date')
